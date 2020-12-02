e38d18b7fc51d7f55c7f967c53e13fec8d3b9515
Júlia Lourenzoni <julourenzoni@outlook.com>
2020-12-01 23:07:38 -0300

    parte 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map: google.maps.Map;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
   constructor() {}
+
+  ionViewWillEnter() {
+    this.exibirMapa();
+  }
+
+  exibirMapa() {
+    const posicao = new google.maps.LatLng(-22.500993, -48.707304);
+    const opcoes = {
+      center: posicao,
+      zoom: 15,
+      disableDefaultUI: true
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+
+    new google.maps.Marker({
+      position: posicao,
+      map: this.map,
+      title: "Supermercado",
+      animation: google.maps.Animation.BOUNCE
+    });
+  }
 
 }
